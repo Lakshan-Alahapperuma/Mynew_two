12a1f5b383808816950a0aaa363ebc0c19c01f3b
Lakshan Alahapperuma <147945056+cst22035@users.noreply.github.com>
2024-06-07 23:37:31 +0530

    newproject12

---
+++ new12.java
@@ -0,0 +1,21 @@
+public class new12 {
+
+    public static void main(String[] args) {
+
+        Boolean condition1 = true;
+        Boolean condition2 = false;
+
+        if (condition1){
+            System.out.println("This condition1 is Correct");
+
+        } else if (condition2){
+            System.out.println("This condition2 is Correct");
+
+        } else {
+            System.out.println("condition1 & condition2 are incorrect");
+        }
+    
+    }
+
+
+}
